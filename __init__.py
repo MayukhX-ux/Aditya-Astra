fdcaa801ed1133c5cab9d8d36f75b88e223d3414
kanak-narware <kanaknarware3110@gmail.com>
2026-07-24 16:25:48 +0530

    test

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from . import axes_size as Size
-from .axes_divider import Divider, SubplotDivider, make_axes_locatable
-from .axes_grid import AxesGrid, Grid, ImageGrid
+from pathlib import Path
 
-from .parasite_axes import host_subplot, host_axes
 
-__all__ = ["Size",
-           "Divider", "SubplotDivider", "make_axes_locatable",
-           "AxesGrid", "Grid", "ImageGrid",
-           "host_subplot", "host_axes"]
+# Check that the test directories exist
+if not (Path(__file__).parent / "baseline_images").exists():
+    raise OSError(
+        'The baseline image directory does not exist. '
+        'This is most likely because the test data is not installed. '
+        'You may need to install matplotlib from source to get the '
+        'test data.')
